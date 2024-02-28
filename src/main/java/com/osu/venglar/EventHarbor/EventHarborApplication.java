0f1d902d31e03dca97779d1a03ccbb71b2abf5a0
Matej Venglar <admin.venglar@cihelni.local>
2024-02-28 19:38:15 +0100

    Initial commit

---
+++ src/main/java/com/osu/venglar/EventHarbor/EventHarborApplication.java
@@ -0,0 +1,13 @@
+package com.osu.venglar.EventHarbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHarborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHarborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osu/venglar/EventHarbor/EventHarborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osu.venglar.EventHarbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHarborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
