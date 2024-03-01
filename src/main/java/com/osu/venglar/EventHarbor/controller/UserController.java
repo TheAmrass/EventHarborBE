a30e0e2da8b099d57134b01a5102ae6d27d44b78
Matej Venglar <admin.venglar@cihelni.local>
2024-03-01 15:43:29 +0100

    User completed

--- src/main/java/com/osu/venglar/EventHarbor/controller/UserController.java
+++ src/main/java/com/osu/venglar/EventHarbor/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.osu.venglar.EventHarbor.controller;
 
+import com.osu.venglar.EventHarbor.exception.UserNotFoundException;
 import com.osu.venglar.EventHarbor.model.User;
 import com.osu.venglar.EventHarbor.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+//Connection with FE
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
@@ -17,4 +20,37 @@ public class UserController {
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
     }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setName(newUser.getName());
+                    user.setPassword(newUser.getPassword());
+                    user.setRole(newUser.getRole());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "Uzivatel s id "+id+"byl uspesne odstranen.";
+    }
+
 }
